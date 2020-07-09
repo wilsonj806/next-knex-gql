111b2c51066609d9a64a0740c05660b6af725bc6
Wilson <wilsonj806@gmail.com>
2020-07-09 15:44:15 -0400

    Add database to graphql route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   distDir: 'build',
+
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
 }
--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,4 +1,27 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+import knex from 'knex'
+
+const knexClient = knex({
+  client: "postgres",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+  }
+});
+
+// FIXME use hasTable instead of createTableIfNotExists
+knexClient.schema.createTableIfNotExists('users', (table) => {
+  table.increments('id')
+  table.string('firstName')
+}).then(() => knexClient('users').insert([
+  {firstName: 'Wilson'},
+  {firstName: 'Calvin'}
+]))
+.catch(err => console.log(err));
+
+
 
 const typeDefs = gql`
   type Query {
@@ -10,10 +33,11 @@ const typeDefs = gql`
   }
 `
 
+// TODO integrate Knex into this for resolving shit
 const resolvers = {
   Query: {
-    users(parent, args, context) {
-      return [{ firstName: 'Nextjs' }]
+    users(parent, args, context, resolveInfo) {
+      return knexClient.select('*').from('users')
     },
   },
 }
